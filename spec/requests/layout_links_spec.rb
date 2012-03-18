63034fb2c6bc49558c289d5823dc902fecaa9787
Kathleen Schadeck <kschadeck@gmail.com>
2012-03-17 22:24:02 -0500

    Added users controller and completed Exercise Ch5 -Lab 6-1

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe "LayoutLinks" do
   
+  
   it "should have a Home page at '/'" do
     get '/'
     #response.should have_selector('title', :content => "Home")
@@ -28,6 +29,25 @@ describe "LayoutLinks" do
     #response.should have_selector('title', :content => "Help")
     response.should have_selector("title",
                    :content => "Ruby on Rails Tutorial Sample App | Help")
+  end
+  
+  it "should have a signup page at '/signup'" do
+    get '/signup'
+    response.should have_selector('title', :content => "Sign up")
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    response.should have_selector('title', :content => "About")
+    click_link "Help"
+    response.should have_selector('title', :content => "Help")
+    click_link "Contact"
+    response.should have_selector('title', :content => "Contact")
+    click_link "Home"
+    response.should have_selector('title', :content => "Home")
+    click_link "Sign up now!"
+    response.should have_selector('title', :content => "Sign up")
   end
 
   #describe "GET /layout_links" do
