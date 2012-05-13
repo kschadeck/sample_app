5df89dfccefbb33082f978aed9cb5926d1e4ac9d
Kathleen Schadeck <kschadeck@gmail.com>
2012-05-13 00:13:17 -0500

    Done with sign in - chapter 9

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -5,28 +5,28 @@ describe "LayoutLinks" do
   
   it "should have a Home page at '/'" do
     get '/'
-    #response.should have_selector('title', :content => "Home")
+    response.should have_selector('title', :content => "Home")
     response.should have_selector("title",
                    :content => "Ruby on Rails Tutorial Sample App | Home")
   end
 
   it "should have a Contact page at '/contact'" do
     get '/contact'
-    #response.should have_selector('title', :content => "Contact")
+    response.should have_selector('title', :content => "Contact")
     response.should have_selector("title",
                    :content => "Ruby on Rails Tutorial Sample App | Contact")
   end
 
   it "should have an About page at '/about'" do
     get '/about'
-    #response.should have_selector('title', :content => "About")
+    response.should have_selector('title', :content => "About")
     response.should have_selector("title",
                    :content => "Ruby on Rails Tutorial Sample App | About")
   end
 
   it "should have a Help page at '/help'" do
     get '/help'
-    #response.should have_selector('title', :content => "Help")
+    response.should have_selector('title', :content => "Help")
     response.should have_selector("title",
                    :content => "Ruby on Rails Tutorial Sample App | Help")
   end
@@ -49,4 +49,38 @@ describe "LayoutLinks" do
     click_link "Sign up now!"
     response.should have_selector('title', :content => "Sign up")
   end
+  
+  
+  #describe "when not signed in" do
+  #  it "should have a signin link" do
+  #    visit root_path
+  #    response.should have_selector("a", :href => signin_path,
+  #                                       :content => "Sign in")
+  #  end
+  #end
+
+  describe "when signed in" do
+
+    before(:each) do
+      @user = Factory(:user)
+      visit signin_path
+      fill_in :email,    :with => @user.email
+      fill_in :password, :with => @user.password
+      click_button
+    end
+
+    #it "should have a signout link" do
+    #  visit root_path
+    #  response.should have_selector("a", :href => signout_path,
+    #                                     :content => "Sign out")
+    #end
+
+    #it "should have a profile link" do
+    #  visit root_path
+    #  response.should have_selector("a", :href => user_path(@user),
+    #                                     :content => "Profile")
+    #end
+ 
+  end
+
 end
